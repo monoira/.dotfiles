5f4f9aa64d17d6c9dec735e672ab90335f42615e
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-05-06 21:39:21 +0400

    upd

--- install_packages.sh
+++ install_packages.sh
@@ -30,8 +30,6 @@ install_yay_packages_and_check() {
 
 install_pacman_packages_and_check zsh
 install_pacman_packages_and_check zsh-syntax-highlighting
-# Set zsh as the default shell
-sudo chsh -s /bin/zsh
 
 install_pacman_packages_and_check timeshift
 install_pacman_packages_and_check nodejs
