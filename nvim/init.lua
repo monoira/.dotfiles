3db594ec571ff234fcfbe66a99ef887d68d0bfa8
Irakli Tatarashvili <116330974+monoira@users.noreply.github.com>
2024-05-07 11:47:00 +0400

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,3 +7,23 @@ require("config.lazy")
 vim.opt.title = true
 vim.opt.titlestring = "%-25.55F %a%r%m"
 vim.opt.titlelen = 70
+
+-- HACK: needed for wsl2 clipboard to work on windows
+local clip = "/mnt/c/Windows/System32/clip.exe" -- Change this path if needed
+
+if vim.fn.executable(clip) then
+  local opts = {
+    callback = function()
+      if vim.v.event.operator ~= "y" then
+        return
+      end
+      vim.fn.system(clip, vim.fn.getreg(0))
+    end,
+  }
+
+  opts.group = vim.api.nvim_create_augroup("WSLYank", {})
+  vim.api.nvim_create_autocmd(
+    "TextYankPost",
+    { group = opts.group, callback = opts.callback }
+  )
+end
