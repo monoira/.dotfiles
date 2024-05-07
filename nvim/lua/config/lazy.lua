bd678a193964d36cf1556d7266e1f565fd9ada21
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-05-07 23:56:31 +0400

    upd

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -27,6 +27,9 @@ require("lazy").setup({
 
     { import = "lazyvim.plugins.extras.lazyrc" },
 
+    -- neotest
+    { import = "lazyvim.plugins.extras.test.core" },
+
     -- debugger
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.dap.nlua" },
---
+++ nvim/lua/plugins/neotest.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/neotest-jest",
+      "nvim-neotest/neotest-go",
+
+      -- needed.
+      "nvim-treesitter/nvim-treesitter",
+    },
+  },
+}
