08f6107dcbb83aa0692aff9cdc4c965863ca03b2
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-05-10 13:57:05 +0400

    upd

--- install_packages.sh
+++ install_packages.sh
@@ -7,39 +7,40 @@ log_file=~/install_progress_log.txt
 
 # Function to check if a package is installed
 package_installed() {
-    if command -v "$1" &>/dev/null; then
-        return 0  # Package is installed
-    else
-        return 1  # Package is not installed
-    fi
+	if command -v "$1" &>/dev/null; then
+		return 0 # Package is installed
+	else
+		return 1 # Package is not installed
+	fi
 }
 
 # Function to install Yay packages and check installation
 install_yay_package_and_check() {
-    package_name=$1
-    yay -S --noconfirm "$package_name"
-    if package_installed "$package_name"; then
-        echo "$package_name Installed" >> "$log_file"
-    else
-        echo "$package_name FAILED TO INSTALL!!!" >> "$log_file"
-    fi
+	package_name=$1
+	yay -S --noconfirm "$package_name"
+	if package_installed "$package_name"; then
+		echo "$package_name Installed" >>"$log_file"
+	else
+		echo "$package_name FAILED TO INSTALL!!!" >>"$log_file"
+	fi
 }
 
 # Function to install Pacman packages and check installation
 install_pacman_package_and_check() {
-    package_name=$1
-    sudo pacman -S --noconfirm "$package_name"
-    if package_installed "$package_name"; then
-        echo "$package_name Installed" >> "$log_file"
-    else
-        echo "$package_name FAILED TO INSTALL!!!" >> "$log_file"
-    fi
+	package_name=$1
+	sudo pacman -S --noconfirm "$package_name"
+	if package_installed "$package_name"; then
+		echo "$package_name Installed" >>"$log_file"
+	else
+		echo "$package_name FAILED TO INSTALL!!!" >>"$log_file"
+	fi
 }
 
 # YAY AUR PACKAGES
 yay --noconfirm
 install_yay_package_and_check visual-studio-code-bin
 install_yay_package_and_check figma-linux
+install_yay_package_and_check postman-bin
 
 # PACMAN PACKAGES
 install_pacman_package_and_check zsh
@@ -50,10 +51,6 @@ install_pacman_package_and_check npm
 install_pacman_package_and_check git
 install_pacman_package_and_check lazygit
 
-install_pacman_package_and_check nuget
-install_pacman_package_and_check dotnet-sdk
-install_pacman_package_and_check aspnet-runtime
-
 install_pacman_package_and_check xclip
 install_pacman_package_and_check ripgrep
 install_pacman_package_and_check fd
@@ -62,3 +59,5 @@ install_pacman_package_and_check ffmpegthumbs
 install_pacman_package_and_check neovim
 install_pacman_package_and_check otf-commit-mono-nerd
 install_pacman_package_and_check wezterm
+
+install_pacman_package_and_check fastfetch
