fb5eb270b6c5045ce4a69af197c6930a8f7358ca
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-05-11 21:21:29 +0400

    upd

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -12,28 +12,21 @@ return {
       require("colorizer").setup({
         filetypes = { "*" },
         user_default_options = {
-          RGB = true, -- #RGB hex codes
-          RRGGBB = true, -- #RRGGBB hex codes
-          names = true, -- "Name" codes like Blue or blue
-          RRGGBBAA = true, -- #RRGGBBAA hex codes
-          AARRGGBB = true, -- 0xAARRGGBB hex codes
-          rgb_fn = true, -- CSS rgb() and rgba() functions
-          hsl_fn = true, -- CSS hsl() and hsla() functions
-          css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-          css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-          -- Available modes for `mode`: foreground, background,  virtualtext
-          mode = "background", -- Set the display mode.
-          -- Available methods are true/ true / "normal" / "lsp" / "both"
-          -- True is same as normal
-          tailwind = true, -- Enable tailwind colors
-          -- parsers can contain values used in |user_default_options|
-          sass = { enable = true, parsers = { "css" } }, -- Enable sass colors
+          RGB = true,
+          RRGGBB = true,
+          names = true,
+          RRGGBBAA = true,
+          AARRGGBB = true,
+          rgb_fn = true,
+          hsl_fn = true,
+          css = true,
+          css_fn = true,
+          mode = "background",
+          tailwind = true,
+          sass = { enable = true, parsers = { "css" } },
           virtualtext = "■",
-          -- update color values even if buffer is not focused
-          -- example use: cmp_menu, cmp_docs
           always_update = true,
         },
-        -- all the sub-options of filetypes apply to buftypes
         buftypes = {},
       })
     end,
