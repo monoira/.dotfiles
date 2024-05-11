0f82f2760a4e2e7eec700d0706e1c48c635a1901
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-05-11 20:50:49 +0400

    upd

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,10 +1,8 @@
 -- bottom line
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = function(_, opts)
-      opts.sections.lualine_z = {}
-    end,
-  },
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = function(_, opts)
+    opts.sections.lualine_z = {}
+  end,
 }
