9cbb8c2837a14ff4847270940d041bb1b3645eef
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-05-17 21:51:52 +0400

    upd

---
+++ nvim/lua/plugins/0indent-blankline.lua
@@ -0,0 +1,8 @@
+-- https://github.com/LazyVim/LazyVim/issues/3192
+-- As mentioned in the above comment, the problem arises from the incompatibility of the Neovim version. So, waiting for a while should fix the issue. However, if you want to fix it temporarily, you can add the following line to your custom plugins to downgrade the version of the problem plugin.
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  event = "LazyFile",
+  opts = { scope = { enabled = false } },
+  version = "=3.5.4",
+}
