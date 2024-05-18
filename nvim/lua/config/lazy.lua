628082b001295af3ac0cdff66cbe991085750b14
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-05-18 20:40:52 +0400

    upd

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -25,7 +25,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
 
     { import = "lazyvim.plugins.extras.util.project" },
-    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.lazyrc" },
 
     -- debugger
