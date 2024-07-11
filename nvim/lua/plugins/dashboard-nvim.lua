d51a911a0d2ef58c3001498e76e17477a05ba867
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-07-11 17:14:46 +0400

    upd

--- nvim/lua/plugins/dashboard-nvim.lua
+++ nvim/lua/plugins/dashboard-nvim.lua
@@ -1,6 +1,6 @@
 return {
   "nvimdev/dashboard-nvim",
-  event = "VimEnter",
+  lazy = false, -- As https://github.com/nvimdev/dashboard-nvim/pull/450, dashboard-nvim shouldn't be lazy-loaded to properly handle stdin.
   opts = function()
     local logo = [[
        ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆             
@@ -21,10 +21,13 @@ return {
     local opts = {
       theme = "doom",
       hide = {
+        -- this is taken care of by lualine
+        -- enabling this messes up the actual laststatus setting after loading a file
         statusline = false,
       },
       config = {
         header = vim.split(logo, "\n"),
+        -- stylua: ignore
         center = {
           {
             action = 'lua require("persistence").load()',
@@ -70,13 +73,15 @@ return {
       button.key_format = "  %s"
     end
 
-    -- close Lazy and re-open when the dashboard is ready
+    -- open dashboard after closing lazy
     if vim.o.filetype == "lazy" then
-      vim.cmd.close()
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "DashboardLoaded",
+      vim.api.nvim_create_autocmd("WinClosed", {
+        pattern = tostring(vim.api.nvim_get_current_win()),
+        once = true,
         callback = function()
-          require("lazy").show()
+          vim.schedule(function()
+            vim.api.nvim_exec_autocmds("UIEnter", { group = "dashboard" })
+          end)
         end,
       })
     end
