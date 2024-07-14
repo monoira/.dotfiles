497d7e24d44b7d9982b23f068e8d5a688276b80c
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-07-14 15:32:09 +0400

    upd

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,13 +1,5 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    -- NOTE: time it takes for which_key menu to appear
-    vim.o.timeoutlen = 1000
-  end,
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-  },
+  opts = { delay = 1000 },
 }
