21f3266438137e72d5c4b29db98cfe476953c761
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-07-24 17:19:35 +0400

    upd

---
+++ nvim/lua/plugins/grugfar.lua
@@ -0,0 +1,25 @@
+return {
+  "MagicDuck/grug-far.nvim",
+  opts = { headerMaxWidth = 80 },
+  cmd = "GrugFar",
+  keys = {
+    {
+      "<leader>sr",
+      function()
+        local grug = require("grug-far")
+        local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+        grug.grug_far({
+          transient = true,
+          prefills = {
+            search = "",
+            replacement = "",
+            filesFilter = "!node_modules !dist !.eslintcache !*json !*mock* !*mdx !*stories* !*test*",
+            flags = "",
+          },
+        })
+      end,
+      mode = { "n", "v" },
+      desc = "Search and Replace",
+    },
+  },
+}
