16a907b8d7d891d134fa52acf9eb3d130c51a00c
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-09-25 11:13:14 +0400

    upd

--- ubuntu_programs_installer.sh
+++ ubuntu_programs_installer.sh
@@ -4,24 +4,24 @@
 
 # Create a temporary directory
 # TEMP_DIR=$(mktemp -d)
-# 
+#
 # nerd_font_name="CommitMono"
-# 
+#
 # nerd_fonts_repo_url="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$nerd_font_name.zip"
-# 
+#
 # # Download the font zip file
 # wget -O "$TEMP_DIR/font.zip" "$nerd_fonts_repo_url"
-# 
+#
 # # Unzip the font file
 # unzip "$TEMP_DIR/font.zip" -d "$TEMP_DIR"
-# 
+#
 # # Move the font files to the system fonts directory
 # sudo mkdir -p /usr/local/share/fonts/
 # sudo mv "$TEMP_DIR"/*.{ttf,otf} /usr/local/share/fonts/
-# 
+#
 # # Update the font cache
 # fc-cache -f -v
-# 
+#
 # # Clean up
 # rm -rf "$TEMP_DIR"
 
@@ -34,42 +34,46 @@
 # to get information about a single package - NOTE:snap info package_name
 # By default, Snap saves one older version of the Snap packages you can go back to the previous one with the revert option - NOTE:sudo snap revert package_name
 
-
-
 # more info:
 # 1. have to add nerd font manually for now.
 
+# NOTE: || apt packages:
+sudo apt install curl
+sudo apt install python3
+sudo apt install nodejs
+sudo apt install npm
 
-# NOTE: apt programs: 
 sudo apt install alacritty
 
 sudo apt install fzf
 sudo apt install fd-find
 sudo apt install ripgrep
 
-
-sudo apt install xclip wl-clipboard
-
+# needed for neovim - clipboard for - X11
+sudo apt install xclip
+# needed for neovim - clipboard for - wayland
 sudo apt install wl-clipboard
 
+# NOTE: || snap packages:
 
-# PPA installs
-sudo add-apt-repository ppa:lazygit-team/daily
-
-sudo apt update
-
-sudo apt install lazygit
-
-# zsh installation with it's plugins
-sudo apt install zsh
-
-sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-
-
-# NOTE: snap programs:
+# needs --classic flag.
 sudo snap install nvim --classic
+sudo snap install go --classic
+
+# NOTE: ||| COMPLICATED INSTALLATIONS
+
+# lazygit installation
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+rm -rf lazygit.tar.gz
+
+# zsh, ohmyzsh and zsh plugins installations.
+# when ohmyzsh gets installed, it changes shell and pauses this script.
+# when exit is ran, script continues.
+sudo apt install zsh 
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+exit
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
