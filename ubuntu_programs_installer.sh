d616b3d82887dba8f92179e02890f571351a9025
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-09-25 21:40:51 +0400

    Wed Sep 25 09:40:51 PM +04 2024

--- ubuntu_programs_installer.sh
+++ ubuntu_programs_installer.sh
@@ -3,39 +3,38 @@
 # NOTE: Font installation
 
 # Create a temporary directory
-# TEMP_DIR=$(mktemp -d)
-#
-# nerd_font_name="CommitMono"
-#
-# nerd_fonts_repo_url="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$nerd_font_name.zip"
-#
-# # Download the font zip file
-# wget -O "$TEMP_DIR/font.zip" "$nerd_fonts_repo_url"
-#
-# # Unzip the font file
-# unzip "$TEMP_DIR/font.zip" -d "$TEMP_DIR"
-#
-# # Move the font files to the system fonts directory
-# sudo mkdir -p /usr/local/share/fonts/
-# sudo mv "$TEMP_DIR"/*.{ttf,otf} /usr/local/share/fonts/
-#
-# # Update the font cache
-# fc-cache -f -v
-#
-# # Clean up
-# rm -rf "$TEMP_DIR"
+TEMP_DIR=$(mktemp -d)
+
+nerd_font_name="CommitMono"
+
+nerd_fonts_repo_url="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$nerd_font_name.zip"
+
+# Download the font zip file
+wget -O "$TEMP_DIR/font.zip" "$nerd_fonts_repo_url"
+
+# Unzip the font file
+unzip "$TEMP_DIR/font.zip" -d "$TEMP_DIR"
+
+# Move the font files to the system fonts directory
+sudo mkdir -p /usr/local/share/fonts/
+sudo mv "$TEMP_DIR"/*.otf /usr/local/share/fonts/
+
+# Update the font cache
+fc-cache -f -v
+
+# Clean up
+sudo rm -rf "$TEMP_DIR"
 
 # | temporary snap / apt tutorial
-# install with snap / apt - NOTE:sudo snap install package_name
-# remove with snap / apt - NOTE:sudo snap remove package_name
-# | SNAP ONLY
-# see all available updates with snap - NOTE:sudo snap refresh --list
-# update with snap - NOTE:sudo snap refresh
-# to get information about a single package - NOTE:snap info package_name
-# By default, Snap saves one older version of the Snap packages you can go back to the previous one with the revert option - NOTE:sudo snap revert package_name
+# install --- sudo snap install package_name
+# remove --- sudo snap remove package_name
 
-# more info:
-# 1. have to add nerd font manually for now.
+# | SNAP ONLY
+# update all snaps - sudo snap refresh
+# see all available updates with snap --- sudo snap refresh --list
+# update with snap --- sudo snap refresh
+# to get information about a single package --- snap info package_name
+# By default, Snap saves one older version of the Snap packages you can go back to the previous one with the revert option --- sudo snap revert package_name
 
 # NOTE: || apt packages:
 sudo apt install curl
@@ -45,7 +44,7 @@ sudo apt install nodejs
 sudo apt install npm
 
 sudo apt install alacritty
-sudo apt install zsh 
+sudo apt install zsh
 
 sudo apt install fzf
 sudo apt install fd-find
