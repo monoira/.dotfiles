4f063a056ba4a985c1c85c81990072ed15bed8c3
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-09-26 22:12:21 +0400

    UPDATE: Thu Sep 26 10:12:21 PM +04 2024

--- ubuntu_programs_installer.sh
+++ ubuntu_programs_installer.sh
@@ -79,6 +79,7 @@ fc-cache -f -v
 sudo rm -rf "$TEMP_FONT_DIR"
 
 # NOTE: || THOSE MUST BE DONE MANUALLY!
+
 # sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 # git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
