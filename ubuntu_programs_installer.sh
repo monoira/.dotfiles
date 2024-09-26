f685a1c4897cac93f91bea0b3b056d892c0e2183
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-09-26 11:06:51 +0400

    UPDATE: Thu Sep 26 11:06:51 AM +04 2024

--- ubuntu_programs_installer.sh
+++ ubuntu_programs_installer.sh
@@ -27,34 +27,44 @@ fc-cache -f -v
 sudo rm -rf "$TEMP_DIR"
 
 # NOTE: || apt packages:
-sudo apt install curl
-sudo apt install git
-sudo apt install python3
-sudo apt install nodejs
-sudo apt install npm
+sudo apt install -y curl
+sudo apt install -y git
+sudo apt install -y python3
+sudo apt install -y nodejs
+sudo apt install -y npm
 
-sudo apt install alacritty
-sudo apt install zsh
+sudo apt install -y alacritty
+sudo apt install -y zsh
 
-sudo apt install fzf
-sudo apt install fd-find
-sudo apt install ripgrep
+# NOTE: basic programs + needed for neovim
+sudo apt install -y fzf
+sudo apt install -y fd-find
+sudo apt install -y ripgrep
 
+# NOTE: clipboard packages
 # needed for neovim - clipboard for - X11
-sudo apt install xclip
+sudo apt install -y xclip
 # needed for neovim - clipboard for - wayland
-sudo apt install wl-clipboard
+sudo apt install -y wl-clipboard
 
-sudo apt install vlc
-sudo apt install libreoffice
+# NOTE: general packages
+sudo apt install -y vlc
+sudo apt install -y libreoffice
+sudo apt install -y qbittorrent
+sudo apt install -y strawberry
+sudo apt install -y virtualbox
+sudo apt install -y virtualbox-ext-pack
 
 # NOTE: || snap packages:
 
-# needs --classic flag.
-sudo snap install nvim --classic
-sudo snap install go --classic
+# normal snaps
 
-# NOTE: ||| COMPLICATED INSTALLATIONS
+# snaps that need --classic flag.
+sudo snap install --classic nvim
+sudo snap install --classic code
+sudo snap install --classic go
+
+# NOTE: || COMPLICATED INSTALLATIONS
 
 # lazygit installation
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
@@ -64,7 +74,7 @@ sudo install lazygit /usr/local/bin
 rm -rf lazygit.tar.gz
 rm -rf lazygit
 
-# those must be done manually!
+# NOTE: those must be done manually!
 # sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 # git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
