529c11b69f6c1ea4a532aa831981a856201860ad
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-09-26 22:12:09 +0400

    UPDATE: Thu Sep 26 10:12:09 PM +04 2024

--- ubuntu_programs_installer.sh
+++ ubuntu_programs_installer.sh
@@ -1,32 +1,6 @@
 #!/usr/bin/env bash
 
-# NOTE: Automatic font installation
-
-# Create a temporary directory
-TEMP_DIR=$(mktemp --directory)
-
-nerd_font_name="Hack"
-
-nerd_fonts_repo_url="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$nerd_font_name.zip"
-
-# download the font zip file
-wget -O "$TEMP_DIR/font.zip" "$nerd_fonts_repo_url"
-
-# unzip the font file
-unzip "$TEMP_DIR/font.zip" -d "$TEMP_DIR"
-
-# move the font files to the system fonts directory
-sudo mkdir -p /usr/local/share/fonts/
-# BUG: only works if fonts inside zip are .ttf extension. doesn't work with .otf. fix later.
-sudo mv "$TEMP_DIR"/*.ttf /usr/local/share/fonts/
-
-# update the font cache
-fc-cache -f -v
-
-# clean up
-sudo rm -rf "$TEMP_DIR"
-
-# NOTE: || apt packages:
+# NOTE: || APT PACKAGES:
 sudo apt install -y curl
 sudo apt install -y git
 sudo apt install -y python3
@@ -36,18 +10,17 @@ sudo apt install -y npm
 sudo apt install -y alacritty
 sudo apt install -y zsh
 
-# NOTE: basic programs + needed for neovim
+# basic programs + needed for neovim
 sudo apt install -y fzf
 sudo apt install -y fd-find
 sudo apt install -y ripgrep
 
-# NOTE: clipboard packages
 # needed for neovim - clipboard for - X11
 sudo apt install -y xclip
 # needed for neovim - clipboard for - wayland
 sudo apt install -y wl-clipboard
 
-# NOTE: general packages
+# general packages
 sudo apt install -y libreoffice
 sudo apt install -y vlc
 sudo apt install -y timeshift
@@ -58,7 +31,7 @@ sudo apt install -y rclone-browser
 sudo apt install -y virtualbox
 sudo apt install -y virtualbox-ext-pack
 
-# NOTE: || snap packages:
+# NOTE: || SNAP PACKAGES:
 
 # normal snaps
 sudo snap install postman
@@ -69,7 +42,7 @@ sudo snap install --classic nvim
 sudo snap install --classic code
 sudo snap install --classic go
 
-# NOTE: || COMPLICATED INSTALLATIONS
+# NOTE: || COMPLICATED INSTALLATIONS.
 
 # lazygit installation
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
@@ -79,7 +52,33 @@ sudo install lazygit /usr/local/bin
 rm -rf lazygit.tar.gz
 rm -rf lazygit
 
-# NOTE: those must be done manually!
+# NOTE: Automatic font installation
+
+# Create a temporary directory
+TEMP_FONT_DIR=$(mktemp --directory)
+
+nerd_font_name="Hack"
+
+nerd_fonts_repo_url="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$nerd_font_name.zip"
+
+# download the font zip file
+wget -O "$TEMP_FONT_DIR/font.zip" "$nerd_fonts_repo_url"
+
+# unzip the font file
+unzip "$TEMP_FONT_DIR/font.zip" -d "$TEMP_FONT_DIR"
+
+# move the font files to the system fonts directory
+sudo mkdir -p /usr/local/share/fonts/
+# BUG: only works if fonts inside zip are .ttf extension. doesn't work with .otf. fix later.
+sudo mv "$TEMP_FONT_DIR"/*.ttf /usr/local/share/fonts/
+
+# update the font cache
+fc-cache -f -v
+
+# clean up
+sudo rm -rf "$TEMP_FONT_DIR"
+
+# NOTE: || THOSE MUST BE DONE MANUALLY!
 # sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 # git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
