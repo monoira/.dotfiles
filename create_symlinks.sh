c123141ba5d53bb6868b6cd1f08161b7a47e91e6
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-09-29 16:36:45 +0400

    UPDATE: Sun Sep 29 04:36:45 PM +04 2024

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -3,23 +3,24 @@
 # || Variables
 dotfiles_dir=~/.dotfiles
 
-# || removing default configurations that stayed after installations of packages.
-# >/dev/null thing makes it so that any system responses will be written there
+# - First line removes default configurations that stayed after installations of packages.
+# >/dev/null thing makes it so that any system responses will be written there.
+# - Second line creates symlink between files at .dotfiles and it's regular destination.
 
 sudo rm -rf ~/.bashrc >/dev/null 2>&1
-sudo rm -rf ~/.zshrc >/dev/null 2>&1
-sudo rm -rf ~/.bash_aliases >/dev/null 2>&1
-sudo rm -rf ~/.gitconfig >/dev/null 2>&1
-
-sudo rm -rf ~/.config/nvim >/dev/null 2>&1
-sudo rm -rf ~/.config/alacritty >/dev/null 2>&1
-
-# || creating symlinks
-
 ln -sf $dotfiles_dir/.bashrc ~/.bashrc
+
+sudo rm -rf ~/.zshrc >/dev/null 2>&1
 ln -sf $dotfiles_dir/.zshrc ~/.zshrc
+
+sudo rm -rf ~/.bash_aliases >/dev/null 2>&1
 ln -sf $dotfiles_dir/.bash_aliases ~/.bash_aliases
+
+sudo rm -rf ~/.gitconfig >/dev/null 2>&1
 ln -sf $dotfiles_dir/.gitconfig ~/.gitconfig
 
+sudo rm -rf ~/.config/nvim >/dev/null 2>&1
 ln -sf $dotfiles_dir/nvim ~/.config/nvim
+
+sudo rm -rf ~/.config/alacritty >/dev/null 2>&1
 ln -sf $dotfiles_dir/alacritty ~/.config/alacritty
