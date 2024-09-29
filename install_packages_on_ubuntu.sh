cf62e16a347601097a786852d5d08e1f209ea487
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-09-29 21:44:22 +0400

    UPDATE: Sun Sep 29 09:44:22 PM +04 2024

--- install_packages_on_ubuntu.sh
+++ install_packages_on_ubuntu.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # NOTE: || APT PACKAGES:
-sudo apt update && sudo apt upgrade -y
+sudo apt update -y && sudo apt upgrade -y
 sudo apt install -y curl
 sudo apt install -y git
 sudo apt install -y python3
@@ -50,13 +50,40 @@ sudo snap install --classic code
 
 # NOTE: || COMPLICATED INSTALLATIONS.
 
-# lazygit installation
+# NOTE: lazygit installation
+cd /tmp
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
+curl -sLo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar -xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
-rm -rf lazygit.tar.gz
-rm -rf lazygit
+rm lazygit.tar.gz lazygit
+cd -
+
+# NOTE: docker installation
+# Add the official Docker repo
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo wget -qO /etc/apt/keyrings/docker.asc https://download.docker.com/linux/ubuntu/gpg
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+sudo apt update -y
+
+# Install Docker engine and standard plugins
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
+
+# Give this user privileged Docker access
+sudo usermod -aG docker ${USER}
+
+# Limit log size to avoid running out of disk
+echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json
+
+# NOTE: lazydocker installation
+cd /tmp
+LAZYDOCKER_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazydocker/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -sLo lazydocker.tar.gz "https://github.com/jesseduffield/lazydocker/releases/latest/download/lazydocker_${LAZYDOCKER_VERSION}_Linux_x86_64.tar.gz"
+tar -xf lazydocker.tar.gz lazydocker
+sudo install lazydocker /usr/local/bin
+rm lazydocker.tar.gz lazydocker
+cd -
 
 # NOTE: automatic font installation
 
