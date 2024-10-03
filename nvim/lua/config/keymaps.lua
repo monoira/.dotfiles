e5afbc04b9f0ae6e09eeb9cca0e76a37d4bedd73
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-10-03 15:43:57 +0400

    Thu Oct  3 03:43:57 PM +04 2024

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- Panes
+map("n", "<leader>w", "<c-w>", { desc = "Panes", remap = true })
+map("n", "<leader>-", "<C-W>s", { desc = "Split Pane Below", remap = true })
+map("n", "<leader>|", "<C-W>v", { desc = "Split Pane Right", remap = true })
+map("n", "<leader>wd", "<C-W>c", { desc = "Delete Pane", remap = true })
+LazyVim.toggle.map("<leader>wm", LazyVim.toggle.maximize)
