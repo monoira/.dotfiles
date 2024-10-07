8628ed410be2284168705b3f45643d1083362c0e
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-10-07 11:03:37 +0400

    update

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,25 +1,3 @@
--- return {
---   {
---     "akinsho/toggleterm.nvim",
---     config = true,
---     lazy = true,
---     cmd = "ToggleTerm",
---     keys = {
---       {
---         "<leader>\\",
---         "<cmd>:lcd %:h<cr>",
---         "<cmd>ToggleTerm<cr>",
---         desc = "Toggle terminal",
---       },
---     },
---     opts = {
---       open_mapping = "<leader>\\",
---       direction = "horizontal",
---       size = 10,
---     },
---   },
--- }
-
 return {
   {
     "akinsho/toggleterm.nvim",
