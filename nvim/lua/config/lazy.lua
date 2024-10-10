f7efc6de34df9ee7df29b4d72276a7d26b57f4ab
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-10-10 13:57:28 +0400

    Thu Oct 10 01:57:28 PM +04 2024

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -23,18 +23,18 @@ require("lazy").setup({
     -- if you activate this, then change "enable_tailwind = false," to true in init.lua nvim-highlight-colors
     -- { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.lang.markdown" },
-    { import = "lazyvim.plugins.extras.lang.yaml" },
-
     { import = "lazyvim.plugins.extras.lang.go" },
-    { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.sql" },
+
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.lang.git" },
 
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
 
-    { import = "lazyvim.plugins.extras.lang.git" },
     { import = "lazyvim.plugins.extras.util.project" },
 
     { import = "lazyvim.plugins.extras.editor.telescope" },
