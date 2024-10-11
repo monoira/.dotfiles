771631c60c0c4985a5c33c5a2237e5e89faa544f
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-10-11 09:43:49 +0400

    Fri Oct 11 09:43:49 AM +04 2024

--- install_packages_on_ubuntu.sh
+++ install_packages_on_ubuntu.sh
@@ -132,3 +132,32 @@ fi
 # NOTE: installing vimv - cargo required!
 cargo install vimv
 sudo ln -sf ~/.cargo/bin/vimv /bin/vimv
+
+# NOTE: changing gsettings to customize gnome desktop environment to my taste
+
+# hide the trash from dash-to-dock
+gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false
+
+# night-light settings
+gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
+gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-automatic false
+gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-from 20.0
+gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-to 6.0
+gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature 4000
+
+# show apps from current workspace only
+gsettings set org.gnome.shell.app-switcher current-workspace-only true
+gsettings set org.gnome.shell.window-switcher current-workspace-only true
+
+# desktop icons size
+gsettings set org.gnome.shell.extensions.ding icon-size 'small'
+
+# dash-to-dock settings
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 24
+gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height true
+gsettings set org.gnome.desktop.interface clock-show-date false
+
+# disable update notifications
+gsettings set com.ubuntu.update-notifier no-show-notifications true
