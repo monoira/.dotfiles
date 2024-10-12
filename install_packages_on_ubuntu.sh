70a5ce06f7d7ef3033794bbf010e16a09f428ff1
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-10-12 18:33:08 +0400

    Sat Oct 12 06:33:08 PM +04 2024

--- install_packages_on_ubuntu.sh
+++ install_packages_on_ubuntu.sh
@@ -134,22 +134,12 @@ sudo ln -sf ~/.cargo/bin/vimv /bin/vimv
 
 # NOTE: changing gsettings to customize gnome desktop environment to my taste
 
-# hide the trash from dash-to-dock
-gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false
-
 # night-light settings
 gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
 gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-automatic false
 gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-from 20.0
 gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-to 6.0
 gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature 4000
-
-# show apps from current workspace only
-gsettings set org.gnome.shell.app-switcher current-workspace-only true
-gsettings set org.gnome.shell.window-switcher current-workspace-only true
-
-# desktop icons size
-gsettings set org.gnome.shell.extensions.ding icon-size 'small'
 
 # dash-to-dock settings
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 24
@@ -158,5 +148,19 @@ gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
 gsettings set org.gnome.shell.extensions.dash-to-dock extend-height true
 gsettings set org.gnome.desktop.interface clock-show-date false
 
+# show apps from current workspace only
+gsettings set org.gnome.shell.app-switcher current-workspace-only true
+gsettings set org.gnome.shell.window-switcher current-workspace-only true
+
+# hide the trash from dash-to-dock
+gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false
+
+# desktop icons size
+gsettings set org.gnome.shell.extensions.ding icon-size 'small'
+# hides home directory on desktop
+gsettings set org.gnome.shell.extensions.ding show-home false
+
 # disable update notifications
 gsettings set com.ubuntu.update-notifier no-show-notifications true
+# disables notification banners
+gsettings set org.gnome.desktop.notifications show-banners true
