8c91f95606d07a66cb8f74baede86925d777e939
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-10-24 11:13:48 +0400

    Thu Oct 24 11:13:48 AM +04 2024

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -3,24 +3,28 @@
 # || Variables
 dotfiles_dir=~/.dotfiles
 
+# - First line removes default configurations that stayed after installations of packages.
+# >/dev/null thing makes it so that any system responses will be written there.
+# - Second line creates symlink between files at .dotfiles and it's regular destination.
+
 # NOTE: if you add file or directory here, also add it in uninstall.sh
 
 sh ./uninstall.sh
 
 # single file dotfiles
 
-sudo rm -rf ~/.bashrc >/dev/null 2>&1
+ln -sf $dotfiles_dir/zsh/.bashrc ~/.bashrc
 
-sudo rm -rf ~/.zshrc >/dev/null 2>&1
+ln -sf $dotfiles_dir/zsh/.zshrc ~/.zshrc
 
-sudo rm -rf ~/.zsh_aliases >/dev/null 2>&1
+ln -sf $dotfiles_dir/zsh/.zsh_aliases ~/.zsh_aliases
 
-sudo rm -rf ~/.gitconfig >/dev/null 2>&1
+ln -sf $dotfiles_dir/.gitconfig ~/.gitconfig
 
 # directories
 
-sudo rm -rf ~/.config/nvim >/dev/null 2>&1
+ln -sf $dotfiles_dir/nvim ~/.config/nvim
 
-sudo rm -rf ~/.config/alacritty >/dev/null 2>&1
+ln -sf $dotfiles_dir/alacritty ~/.config/alacritty
 
-sudo rm -rf ~/.config/tmux >/dev/null 2>&1
+ln -sf $dotfiles_dir/tmux ~/.config/tmux
