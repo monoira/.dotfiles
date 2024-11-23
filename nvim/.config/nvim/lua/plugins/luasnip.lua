bd69b59d228f4b23a3e190cbd3c67a79e6a396e2
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-11-23 23:04:33 +0400

    Sat Nov 23 11:04:33 PM +04 2024

--- nvim/.config/nvim/lua/plugins/luasnip.lua
+++ nvim/.config/nvim/lua/plugins/luasnip.lua
@@ -1,15 +1,8 @@
 return {
   "L3MON4D3/LuaSnip",
-  dependencies = {
-    {
-      "rafamadriz/friendly-snippets",
-      config = function()
-        require("luasnip.loaders.from_vscode").lazy_load({ paths = "~/.config/nvim/snippets" })
-      end,
-    },
-  },
   opts = {
     history = true,
     delete_check_events = "TextChanged",
   },
+  require("luasnip.loaders.from_vscode").lazy_load({ paths = "~/.config/nvim/snippets" }),
 }
