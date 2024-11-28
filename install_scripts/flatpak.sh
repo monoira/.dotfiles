a102bc46ea3740eedbadcc886607a92fc2b7461c
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-11-28 20:17:36 +0400

    Thu Nov 28 08:17:36 PM +04 2024

--- install_scripts/flatpak.sh
+++ install_scripts/flatpak.sh
@@ -2,5 +2,4 @@
 
 echo "<--- installing flatpak... --->"
 sudo apt install -y flatpak
-sudo apt install -y gnome-software-plugin-flatpak
-sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
