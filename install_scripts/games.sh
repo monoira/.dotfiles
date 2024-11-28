cef685e931539673730f0ad7ea75ec429aa2363a
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-11-28 18:59:22 +0400

    Thu Nov 28 06:59:20 PM +04 2024

--- install_scripts/games.sh
+++ install_scripts/games.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+# NOTE: To ensure compatibility with 32-bit and 64-bit games in Steam’s extensive library, enable 32-bit support.
+# Lower-end systems, which often run 32-bit games, particularly benefit from this support.
+# Meanwhile, high-powered systems experience no negative impact from installing additional packages.
+# To enable 32-bit support on your Ubuntu system, run the following command:
+sudo dpkg --add-architecture i386
 
-echo "<--- installing heroicgameslauncher"
+echo "<--- installing heroicgameslauncher... --->"
 flatpak install flathub com.heroicgameslauncher.hgl
+
+echo "<--- installing Steam... --->"
+snap install steam
