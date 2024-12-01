84324c4cc119d413988a6612850c050dbd9cc661
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-12-01 17:20:52 +0400

    Sun Dec  1 05:20:52 PM +04 2024

--- install_scripts/games.sh
+++ install_scripts/games.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env bash
 # NOTE: To ensure compatibility with 32-bit and 64-bit games in Steam’s extensive library, enable 32-bit support.
-# Lower-end systems, which often run 32-bit games, particularly benefit from this support.
-# Meanwhile, high-powered systems experience no negative impact from installing additional packages.
-# To enable 32-bit support on your Ubuntu system, run the following command:
-sudo dpkg --add-architecture i386 && sudo apt -y update && sudo apt -y upgrade
+# ALSO NEEDED FOR HEROIC GAMES LAUNCHER IF NOT USING flatpak version.
+sudo dpkg --add-architecture i386 && sudo apt -y update && sudo apt install -y wine64 wine32 libasound2-plugins:i386 libsdl2-2.0-0:i386 libdbus-1-3:i386 libsqlite3-0:i386
 
-echo "<--- installing lutris... --->"
-sudo apt install -y lutris
+echo "<--- installing Heroic Games Launcher... --->"
+
+cd /tmp || exit
+
+# Get the latest release version
+HEROIC_VERSION=$(curl -s "https://api.github.com/repos/Heroic-Games-Launcher/HeroicGamesLauncher/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+
+# Download the .deb package
+curl -sLo "heroic_${HEROIC_VERSION}_amd64.deb" "https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/latest/download/heroic_${HEROIC_VERSION}_amd64.deb"
+
+# Install the .deb package
+sudo dpkg -i "heroic_${HEROIC_VERSION}_amd64.deb"
+
+# Clean up
+rm "heroic_${HEROIC_VERSION}_amd64.deb"
+
+# Return to the original directory
+cd - || exit
 
 echo "<--- installing Steam... --->"
 sudo snap install steam
