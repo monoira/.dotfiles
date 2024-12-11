32cf6122f09dca4a28f48c093fc91b3661df3be3
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-12-11 20:26:47 +0400

    refactored luasnip

--- nvim/.config/nvim/lua/plugins/luasnip.lua
+++ nvim/.config/nvim/lua/plugins/luasnip.lua
@@ -1,8 +1,15 @@
 return {
   "L3MON4D3/LuaSnip",
+  lazy = true,
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+      require("luasnip.loaders.from_vscode").lazy_load({ paths = "~/.config/nvim/snippets" })
+    end,
+  },
   opts = {
     history = true,
     delete_check_events = "TextChanged",
   },
-  require("luasnip.loaders.from_vscode").lazy_load({ paths = "~/.config/nvim/snippets" }),
 }
