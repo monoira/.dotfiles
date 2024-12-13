4d3a5f0a7b2cc9adcd24e70f234ee3a6a487cbb7
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-12-13 10:59:51 +0400

    refactored noice and changed cmdline to neovim's default bottom one.

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -1,13 +1,18 @@
--- this configuration change hides auto ctrl+k hint box popups that annoy you while typing.
 return {
   "folke/noice.nvim",
   opts = {
     lsp = {
+      -- NOTE: this configuration change hides auto ctrl+k hint box popups that annoy you while typing.
       signature = {
         auto_open = {
           enabled = false,
         },
       },
+    },
+
+    -- NOTE: Classic Cmdline
+    cmdline = {
+      view = "cmdline",
     },
   },
 }
