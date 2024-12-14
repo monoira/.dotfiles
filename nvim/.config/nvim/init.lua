290b517ecb045a4cff2d732f400a99397f63cb0a
Irakli_Tatarashvili <116330974+monoira@users.noreply.github.com.>
2024-12-14 13:06:07 +0400

    Sat Dec 14 01:06:07 PM +04 2024

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,8 +1,8 @@
 require("config.lazy")
 
 -- NOTE: DATABASES
--- this is what it's content is supposed to look like
+-- this is what it's content is supposed to look like in ~/.config/nvim/lua/config/dbs.lua
 -- vim.g.dbs = {
---   { name = "dev", url = "postgres://<username>:<password>@<host>:<port>/<database>" },
+--   { name = "dev", url = "postgres://<USERNAME>:<PASSWORD>@<HOST>:<PORT>/<DATABASE_NAME>" },
 -- }
 require("config.dbs")
