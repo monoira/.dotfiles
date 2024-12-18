55c33265e3bd343905e730d6e2403234bc77e6ed
monoira <monoira@users.noreply.github.com>
2024-12-18 16:13:16 +0400

    updated packages_snap.sh with better error handling and removed typo from tmux.conf

--- install_scripts/packages_snap.sh
+++ install_scripts/packages_snap.sh
@@ -2,7 +2,11 @@
 
 echo "<--- installing snap packages... --->"
 
+# HACK: to symlink, figma-linux needs to be ran and generate figma-linux/current/.local/share
 sudo snap install figma-linux
+if which figma-linux >/dev/null; then
+  figma-linux
+fi
 sudo ln -s "$HOME/.local/share/fonts" "$HOME/snap/figma-linux/current/.local/share/"
 
 # snaps that need --classic flag.
