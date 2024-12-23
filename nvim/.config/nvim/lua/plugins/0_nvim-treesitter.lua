52db7e31c9dcbdb961f2d13cd662a2c85c7bba6b
monoira <monoira@users.noreply.github.com>
2024-12-23 15:36:13 +0400

    fix: removed unnecessary opts from treesitter opts

--- nvim/.config/nvim/lua/plugins/0_nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/0_nvim-treesitter.lua
@@ -1,32 +1,12 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    highlight = { enable = true },
-    indent = { enable = true },
     ensure_installed = {
       -- NOTE: default syntax highlighting packages in LazyVim
       -- http://www.lazyvim.org/plugins/treesitter#nvim-treesitter
 
       -- NOTE: personal additions
       "css",
-    },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<C-space>",
-        node_incremental = "<C-space>",
-        scope_incremental = false,
-        node_decremental = "<bs>",
-      },
-    },
-    textobjects = {
-      move = {
-        enable = true,
-        goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
-        goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
-        goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
-        goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
-      },
     },
   },
 }
