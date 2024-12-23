1d5911ad07cbbd61c22ff92dc85bc8db80618046
monoira <monoira@users.noreply.github.com>
2024-12-23 13:02:26 +0400

    fix: removed unnecessary code

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,8 +1,6 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
-    sources = { "filesystem", "buffers", "git_status", "document_symbols" },
-    open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
     filesystem = {
       bind_to_cwd = true,
       follow_current_file = { enabled = true },
