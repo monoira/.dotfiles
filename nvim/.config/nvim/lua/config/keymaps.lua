7cba2890de5f42a328b59b01ab68941cf0f20c2c
monoira <monoira@users.noreply.github.com>
2025-01-17 23:03:03 +0400

    feat: added useful keymaps to nvim's keymaps.lua

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- keep cursor to center when using ctrl+d and ctrl+u
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- make ctrl+c behave like Escape
+vim.keymap.set("i", "<C-c>", "<Esc>")
