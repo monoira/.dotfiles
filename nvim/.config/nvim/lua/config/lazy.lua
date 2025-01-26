5df1c2cd65989f5a558937d11d271c87a7842fbc
monoira <monoira@users.noreply.github.com>
2025-01-26 15:13:33 +0400

    chore: updated deps and refactored comments

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -57,10 +57,11 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "catppuccin-mocha" } },
+  -- automatically check for plugin updates
   checker = {
     enabled = false, -- check for plugin updates periodically
     notify = false, -- notify on update
-  }, -- automatically check for plugin updates
+  },
   performance = {
     rtp = {
       -- disable some rtp plugins
