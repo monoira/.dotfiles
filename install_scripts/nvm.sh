e2845ba517d76979026d11589e8ee43cf907df33
monoira <monoira@users.noreply.github.com>
2025-03-22 19:22:56 +0400

    feat: fixed packages_flatpak and refactored nvm.sh

--- install_scripts/nvm.sh
+++ install_scripts/nvm.sh
@@ -3,4 +3,5 @@
 echo "<--- installing nvm... --->"
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-nvm install --lts && nvm use --lts
+nvm install --lts
+nvm use --lts
--- install_scripts/packages_flatpak.sh
+++ install_scripts/packages_flatpak.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "<--- installing postman... --->"
-flatpak install flathub -y --user com.getpostman.Postman
+flatpak install -y --user com.getpostman.Postman
