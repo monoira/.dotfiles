61ae6a983ac0e6ccb3d43cb7c637ea920a72735d
monoira <monoira@users.noreply.github.com>
2025-03-25 20:37:52 +0400

    docs: replaced packages.png with better quality one and changed order of installation in games.sh

--- install_scripts/games.sh
+++ install_scripts/games.sh
@@ -2,8 +2,8 @@
 echo "<--- installing steam... --->"
 sudo snap install steam
 
-echo "<--- installing sober... --->"
-flatpak install -y --user https://sober.vinegarhq.org/sober.flatpakref
-
 echo "<--- installing Heroic... --->"
 flatpak install -y --user com.heroicgameslauncher.hgl
+
+echo "<--- installing sober... --->"
+flatpak install -y --user https://sober.vinegarhq.org/sober.flatpakref
