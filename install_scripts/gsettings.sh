030eb9a4321d86013945c2b4e5e46203e30b4a75
monoira <monoira@users.noreply.github.com>
2025-03-26 22:12:22 +0400

    feat: added gsetting

--- install_scripts/gsettings.sh
+++ install_scripts/gsettings.sh
@@ -4,3 +4,6 @@ echo "<--- changing gsettings to customize gnome desktop environment to my taste
 
 # https://github.com/monoira/slickgnome
 wget -qO- https://raw.githubusercontent.com/monoira/slickgnome/main/gnome_settings.sh | bash
+
+# turning on "Accessibility > Large Text" automatically
+gsettings set org.gnome.desktop.interface text-scaling-factor 1.25
