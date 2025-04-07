96aebee264a28c8fdc56df6659c9683b12eae0df
monoira <monoira@users.noreply.github.com>
2025-04-07 11:02:48 +0400

    chore: updated lazyvim and changed styling @ init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -12,6 +12,7 @@ end
 -- NOTE: for databases, create dbs.lua file at
 -- ~/.config/nvim/lua/config/dbs.lua
 -- example of it's content:
+
 -- vim.g.dbs = {
 --   { name = "dev", url = "postgres://USERNAME:PASSWORD@HOST:PORT/DATABASE_NAME" },
 -- }
