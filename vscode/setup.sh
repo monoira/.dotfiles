c5df757da3ed60b58f3c81f30c1e832d3d833f8c
monoira <monoira@users.noreply.github.com>
2025-07-07 18:46:10 +0400

    feat: moved DOTFILES_DIR to it's separate variable

--- vscode/setup.sh
+++ vscode/setup.sh
@@ -2,12 +2,14 @@
 
 # NOTE: setup snippets and settings.json
 
+DOTFILES_DIR="$HOME/.dotfiles"
+
 # -snfT for directories
 for profile in ~/.config/Code/User/profiles/*/; do
   snippets_directory="${profile}snippets"
   rm -rf "$snippets_directory"
-  ln -snfT ~/.dotfiles/nvim/.config/nvim/snippets "$snippets_directory"
+  ln -snfT "$DOTFILES_DIR/nvim/.config/nvim/snippets" "$snippets_directory"
 done
 
 # -sf for files
-ln -sf ~/.dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
+ln -sf "$DOTFILES_DIR/vscode/settings.json" ~/.config/Code/User/settings.json
