5ee6238b2470e0aaebec9abd435e726577e19499
Irakli Tatarashvili <116330974+monoira@users.noreply.github.com>
2025-07-18 22:28:17 +0400

    feat: set org.gnome.desktop.session idle-delay to 0

--- install_scripts/gsettings.sh
+++ install_scripts/gsettings.sh
@@ -5,5 +5,8 @@ echo "<--- changing gsettings to customize gnome desktop environment to my taste
 # https://github.com/monoira/slickgnome
 wget -qO- https://raw.githubusercontent.com/monoira/slickgnome/main/gnome_settings.sh | bash
 
+# disable "screen locking after period of inactivity"
+gsettings set org.gnome.desktop.session idle-delay 0
+
 # turning on "Accessibility > Large Text" automatically
 gsettings set org.gnome.desktop.interface text-scaling-factor 1.25
