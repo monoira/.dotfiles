b5da57364376b24ced6fcf53425524ac97f3c044
Irakli Tatarashvili <116330974+monoira@users.noreply.github.com>
2025-12-02 18:15:15 +0400

    feat: update script

--- install_scripts/postgres_and_dbeaver.sh
+++ install_scripts/postgres_and_dbeaver.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 echo "<--- installing postgresql... --->"
-sudo dnf install postgresql-server postgresql-contrib
-sudo systemctl enable postgresql
-sudo postgresql-setup --initdb --unit postgresql
-sudo systemctl start postgresql
+sudo dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/F-43-x86_64/pgdg-fedora-repo-latest.noarch.rpm
+sudo dnf install -y postgresql18-server
+sudo /usr/pgsql-18/bin/postgresql-18-setup initdb
+sudo systemctl enable postgresql-18
+sudo systemctl start postgresql-18
 
 echo "<--- installing dbeaver... --->"
 flatpak install -y io.dbeaver.DBeaverCommunity
