89d70efa024ed45ddf36efde108d4ca327f7c525
monoira <monoira@users.noreply.github.com>
2025-12-02 13:14:10 +0400

    fix: removed tmux

--- start.sh
+++ start.sh
@@ -31,9 +31,9 @@ if $all_dependency_packages_are_installed; then
 
     bash ~/.dotfiles/install_scripts/_install.sh
     cd ~/.dotfiles || exit 1
-    stow --verbose --adopt cmus git kitty tmux zsh
+    stow --verbose --adopt cmus git kitty zsh
     git add . && git reset --hard
-    stow --verbose --adopt cmus git kitty tmux zsh
+    stow --verbose --adopt cmus git kitty zsh
   }
 
   echo "<--- cloning using SSH... --->"
