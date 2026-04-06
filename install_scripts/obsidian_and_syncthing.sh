c36066a5dca24421751af226ade5064102c8b479
monoira <monoira@users.noreply.github.com>
2026-04-06 23:23:14 +0400

    feat: added auto start for syncthing

--- install_scripts/obsidian_and_syncthing.sh
+++ install_scripts/obsidian_and_syncthing.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 sudo dnf install syncthing -y
+systemctl --user enable syncthing.service
+systemctl --user start syncthing.service
 flatpak install flathub md.obsidian.Obsidian -y
