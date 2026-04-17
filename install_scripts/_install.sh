7b880e001c424fa233b8e91c817edbac3e36dca6
monoira <monoira@users.noreply.github.com>
2026-04-17 23:29:08 +0400

    refactor: improved file namings and installation structure in _install.sh

--- install_scripts/_install.sh
+++ install_scripts/_install.sh
@@ -5,30 +5,28 @@ sudo dnf upgrade -y --refresh && flatpak update -y
 sudo dnf install -y stow git wget
 
 # change gnome settings via gsettings
-bash ~/.dotfiles/install_scripts/gsettings.sh
+bash ~/.dotfiles/install_scripts/set_gsettings.sh
 
 # simple automated installations
 bash ~/.dotfiles/install_scripts/packages.sh
 bash ~/.dotfiles/install_scripts/packages_flatpak.sh
 
 # complicated automated installations
-bash ~/.dotfiles/install_scripts/postgres_and_dbeaver.sh
-bash ~/.dotfiles/install_scripts/lazygit.sh
-bash ~/.dotfiles/install_scripts/docker.sh
-bash ~/.dotfiles/install_scripts/automated_nerd_font.sh
-bash ~/.dotfiles/install_scripts/nvm.sh
-bash ~/.dotfiles/install_scripts/act.sh
-bash ~/.dotfiles/install_scripts/vimv.sh
-bash ~/.dotfiles/install_scripts/vscode.sh
+bash ~/.dotfiles/install_scripts/install_nerd_font.sh
+bash ~/.dotfiles/install_scripts/install_lazygit.sh
+bash ~/.dotfiles/install_scripts/install_docker.sh
+bash ~/.dotfiles/install_scripts/install_nvm.sh
+bash ~/.dotfiles/install_scripts/install_act.sh
+bash ~/.dotfiles/install_scripts/install_vimv.sh
+bash ~/.dotfiles/install_scripts/install_vscode.sh
+bash ~/.dotfiles/install_scripts/install_postgres_and_dbeaver.sh
+bash ~/.dotfiles/install_scripts/install_obsidian_and_syncthing.sh
+
+# flatpak packages and games
+bash ~/.dotfiles/install_scripts/install_games.sh
 
 # clone git repositories
 bash ~/.dotfiles/install_scripts/dev-clone.sh
-
-# flatpak packages and games
-bash ~/.dotfiles/install_scripts/games.sh
-
-# obsidian and syncthing
-bash ~/.dotfiles/obsidian_and_syncthing.sh
 
 # finish
 sudo dnf upgrade -y --refresh && flatpak update -y && sudo dnf autoremove -y
