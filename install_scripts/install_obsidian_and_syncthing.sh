98b72db226a2577ab5cdd15bc8fb7dbc498cddcd
monoira <monoira@users.noreply.github.com>
2026-04-26 10:02:10 +0400

    feat: add obsidian wayland support for fullscreen

--- install_scripts/install_obsidian_and_syncthing.sh
+++ install_scripts/install_obsidian_and_syncthing.sh
@@ -5,4 +5,7 @@ echo "<--- installing syncthing and obsidian... --->"
 sudo dnf install syncthing -y
 systemctl --user enable syncthing.service
 systemctl --user start syncthing.service
+
 flatpak install flathub md.obsidian.Obsidian -y
+# HACK: for now, obsidian doesn't start fullscreen, so I have to use this to force fullscreen
+sudo flatpak override md.obsidian.Obsidian --socket=wayland --env=OBSIDIAN_USE_WAYLAND=1
