ff349638a992cf8b24fe3a9616faabc13fed860e
monoira <monoira@users.noreply.github.com>
2026-05-04 22:38:37 +0400

    feat: improved act installation script

--- install_scripts/install_act.sh
+++ install_scripts/install_act.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
 echo "<--- installing act... --->"
-
-cd /tmp || exit
-wget -qO act.tar.gz https://github.com/nektos/act/releases/latest/download/act_Linux_x86_64.tar.gz
-sudo tar xf act.tar.gz -C /usr/local/bin act
-rm act.tar.gz
-cd - || exit
+sudo dnf -y copr enable goncalossilva/act
+sudo dnf -y install act-cli
